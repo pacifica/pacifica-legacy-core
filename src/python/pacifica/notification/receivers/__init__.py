5f205bc4791c754d553c4a6572e26872cfa37542
Kevin Fox <Kevin.Fox@pnnl.gov>
2013-06-03 14:02:13 -0700

    Add missing file.

---
+++ src/python/pacifica/notification/receivers/__init__.py
@@ -0,0 +1,12 @@
+def collection_name_get(db, chanel, version=None):
+	if version == None:
+		version = db['notification_versions'].find({'_id':chanel}).next()['value']
+	return "%s_notify_%s" %(chanel, int(version))
+
+def notification_version_get(host, port, db, chanel):
+	client = Connection(host, port)
+	db = client[db]
+	scollection = db['notification_versions']
+	doc = scollection.find({'_id':chanel}).next()
+	version = doc['value']
+	return int(version)
