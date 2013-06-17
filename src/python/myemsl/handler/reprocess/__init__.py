8e02dfcf9fe061a7ef34938c244bc99615e65ae6
David ML Brown Jr <david.brown@pnnl.gov>
2013-06-17 14:54:51 -0700

    here's the basic reprocessing service for storing and editing reprocessors

--- src/python/myemsl/handler/reprocess/__init__.py
+++ src/python/myemsl/handler/reprocess/__init__.py
@@ -4,6 +4,7 @@ from mod_python import apache
 from myemsl.service import reprocess
 
 def handler(req):
-	reprocess.reprocess(req.path_info.split('/')[2], req.method, req.read())
+	req.content_type = "text/xml"
+	req.write(reprocess.reprocess(req.user, req.path_info.split('/')[2], req.method, req.read()))
 	return apache.OK
 
--- src/python/myemsl/service/reprocess/__init__.py
+++ src/python/myemsl/service/reprocess/__init__.py
@@ -1,4 +1,73 @@
 #!/usr/bin/python
 
-def reprocess(name, method, script):
-	pass
+import sys
+from myemsl.getconfig import getconfig
+config = getconfig()
+from myemsl.dbconnect import myemsldb_connect
+
+from myemsl.logging import getLogger
+
+logger = getLogger(__name__)
+
+def reprocess(person_id, name, method, xml):
+	select_sql = """
+SELECT
+  definition
+FROM
+  myemsl.reprocessors
+WHERE
+  person_id = %(pid)i and
+  name = %(name)s;
+"""
+	delete_sql = """
+DELETE FROM
+  myemsl.reprocessors
+WHERE
+  person_id = %(pid)i and
+  name = %(name)s;
+"""
+	update_sql = """
+UPDATE
+  myemsl.reprocessors
+SET
+  definition=%(xml)s
+WHERE
+  person_id = %(pid)i and
+  name = %(name)s
+"""
+	insert_sql = """
+INSERT INTO
+  myemsl.reprocessors
+  (
+    person_id,
+    name,
+    definition
+  )
+VALUES
+  (
+    %(pid)i,
+    %(name)s,
+    %(xml)s
+  );
+"""
+	cnx = myemsldb_connect(myemsl_schema_versions=['1.3'])
+	cursor = cnx.cursor()
+	if method == "GET":
+		cursor.execute(select_sql, {'pid':int(person_id), 'name':name})
+		ret = cursor.fetchall()
+		ret = "\n".join(ret[0])
+	if method == "DELETE":
+		cursor.execute(delete_sql, {'pid':int(person_id), 'name':name})
+		ret = ""
+	if method == "POST" or method == "PUT":
+		# TODO validate xml using xsd
+		cursor.execute(select_sql, {'pid':int(person_id), 'name':name})
+		if len(cursor.fetchall()):
+			cursor.execute(update_sql, {'pid':int(person_id), 'name':name, 'xml':xml})
+		else:
+			cursor.execute(insert_sql, {'pid':int(person_id), 'name':name, 'xml':xml})
+		ret = ""
+	cnx.commit()
+	cnx.close()
+	return ret
+		
