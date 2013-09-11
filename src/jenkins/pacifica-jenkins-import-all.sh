71ba20a29cba7f9a67d9fb5ae038a635f472f787
Kevin Fox <Kevin.Fox@pnnl.gov>
2013-09-10 17:13:00 -0700

    Moved jenkins stuff into its own repo.

--- src/jenkins/pacifica-jenkins-import-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "x${JENKINS_CONFIG}" = "x" ]
-then
-	JENKINS_CONFIG=/var/lib/jenkins/config.xml
-fi
-DIR="$(cd "$(dirname "$0")" && pwd)"
-$DIR/pacifica-jenkins-import-jobs.sh
-$DIR/pacifica-jenkins-import-views.py ${JENKINS_CONFIG}
