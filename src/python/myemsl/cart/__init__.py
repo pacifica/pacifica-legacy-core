b7e467d4c0316874c893140250019e45b6fda325
David ML Brown Jr <david.brown@pnnl.gov>
2014-08-18 14:45:24 -0700

    here's a bit to test pylint

--- src/python/myemsl/cart/__init__.py
+++ src/python/myemsl/cart/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
-
+"""
+This is the doc string for myemsl.cartd
+"""
 def cart_item2hash(cart_id, item_id):
-	return "%s-%s" %(cart_id, item_id)
+	"""
+	This is the doc string for myemsl.cartd.cart_item2hash
+	"""
+	return "%s-%s" % (cart_id, item_id)
 
