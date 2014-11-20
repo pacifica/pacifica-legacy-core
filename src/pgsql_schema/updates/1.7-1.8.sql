cac7ea08e2816c286cc4aac4c135fb0781e3e010
dmlb2000 <dmlb2000@gmail.com>
2014-11-20 14:35:44 -0800

    add and update to the schema

---
+++ src/pgsql_schema/updates/1.7-1.8.sql
@@ -0,0 +1,11 @@
+ALTER TABLE eus.proposals add column actual_end_date date;
+CREATE INDEX proposal_actual_end_date_idx on eus.proposals USING btree(actual_end_date);
+ALTER TABLE eus.proposals add column actual_start_date date;
+CREATE INDEX proposal_actual_start_date_idx on eus.proposals USING btree(actual_start_date);
+ALTER TABLE eus.proposals add column closed_date date;
+CREATE INDEX proposal_closed_date_idx on eus.proposals USING btree(closed_date);
+ALTER TABLE eus.instruments add column active_sw boolean default true;
+CREATE INDEX instrument_active_sw_idx on eus.instruments USING btree(active_sw);
+
+update myemsl.system set value = '1.7' where key = 'schema_version';
+
