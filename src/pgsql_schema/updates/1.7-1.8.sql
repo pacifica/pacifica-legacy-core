c0b64f4138cb9bd81613b4b287365558416011c4
dmlb2000 <dmlb2000@gmail.com>
2014-11-24 14:03:22 -0800

    try to get sql update

--- src/pgsql_schema/updates/1.7-1.8.sql
+++ src/pgsql_schema/updates/1.7-1.8.sql
@@ -4,7 +4,7 @@ ALTER TABLE eus.proposals add column actual_start_date date;
 CREATE INDEX proposal_actual_start_date_idx on eus.proposals USING btree(actual_start_date);
 ALTER TABLE eus.proposals add column closed_date date;
 CREATE INDEX proposal_closed_date_idx on eus.proposals USING btree(closed_date);
-ALTER TABLE eus.instruments add column active_sw boolean default true;
+ALTER TABLE eus.instruments add column active_sw character default 'Y';
 CREATE INDEX instrument_active_sw_idx on eus.instruments USING btree(active_sw);
 
 update myemsl.system set value = '1.7' where key = 'schema_version';
