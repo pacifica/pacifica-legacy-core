6eb59c62ddc940c83bcd7f7cdf734f5cd5d4542b
dmlb2000 <dmlb2000@gmail.com>
2014-11-25 10:48:53 -0800

    fix up userinfo handler with pylint too

--- src/python/myemsl/handler/userinfo/__init__.py
+++ src/python/myemsl/handler/userinfo/__init__.py
@@ -1,16 +1,22 @@
+#!/bin/bash -xe
+"""
+User info handler ment to be called from mod_python embedded in apache.
+"""
 from mod_python import apache
 
 from myemsl.service import userinfo
 
 def handler(req):
-	bits = req.path_info.split('/', 3)
-	item_id = bits[2]
-	type = None
-	if len(bits) > 3:
-		type = bits[3]
-		req.content_type = type
-	else:
-		req.content_type = "application/json"
-	userinfo.userinfo(req.user, type, req)
-	return apache.OK
+    """
+    This is the mod python handler to split the url and call service method.
+    """
+    bits = req.path_info.split('/', 3)
+    dtype = None
+    if len(bits) > 2:
+        dtype = bits[2]
+        req.content_type = type
+    else:
+        req.content_type = "application/json"
+    userinfo.userinfo(req.user, dtype, req)
+    return apache.OK
 
