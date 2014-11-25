d2b38c68fddbfe3d6baab4eba13f74e7c6f925ff
dmlb2000 <dmlb2000@gmail.com>
2014-11-25 10:44:07 -0800

    add new stuff to pylint things better

--- src/python/myemsl/service/userinfo/__init__.py
+++ src/python/myemsl/service/userinfo/__init__.py
@@ -1,45 +1,55 @@
 #!/usr/bin/python
-
+"""
+This is the User Info service that does the work of gathering user info from SQL.
+"""
 import os
 import sys
 import errno
 import urllib
 import myemsl.elasticsearch
-from myemsl.getconfig import getconfig
-config = getconfig()
 from myemsl.dbconnect import myemsldb_connect
 from myemsl.brand import brand
 
-from myemsl.logging import getLogger
-
-logger = getLogger(__name__)
-
-def error(type, message, writer):
-    if type == 'text/xml':
+def error(dtype, message, writer):
+    """
+    This is a generic error helper function to output error messages in the appropriate form.
+    """
+    if dtype == 'text/xml':
         writer.write("<?xml version=\"1.0\"?><myemsl><error message=\"%s\"/></myemsl>\n" %(message))
-    elif type == 'text/html':
+    elif dtype == 'text/html':
         brand('header', writer)
         brand('middle', writer)
         writer.write("%s" %(message))
         brand('footer', writer)
-    elif type == 'application/json':
+    elif dtype == 'application/json':
         writer.write("{'error':'"+message+"'}")
     else:
         writer.write(message)
 
 
-def formatdata(type, data, writer):
-    if type == 'application/json':
+def formatdata(dtype, data, writer):
+    """
+    This is the format output function to send data to the user in the requested format.
+    Supports only JSON for now.
+    """
+    if dtype == 'application/json':
         try:
             import json
-        except:
+        except ImportError, ex:
+            print "Unable to import json (%s)"%(ex)
             import simplejson as json
         writer.write(json.dumps(data))
     else:
-        error(type, "Unsupported Accept Type", writer)
+        error(dtype, "Unsupported Accept Type", writer)
 
 def userinfo(user, dtype, writer):
-    type = None
+    """
+    This does the bulk of the SQL to gather the appropriate data to send back to the user.
+    Currently it consists of the:
+     * EUS user information
+     * EUS proposals for the user
+     * EUS instruments for those proposals
+    """
     sql = """
 SELECT
   person_id,
@@ -61,14 +71,14 @@ WHERE
         error(dtype, "multiple users with (%s)"%(user), writer)
     (person_id, network_id, first_name, last_name, email_address, last_change_date) = rows[0]
     data = {
-"person_id": person_id,
-"network_id": network_id,
-"first_name": first_name,
-"last_name": last_name,
-"email_address": email_address,
-"last_change_date": last_change_date,
-"proposals": {},
-"instruments": {}
+        "person_id": person_id,
+        "network_id": network_id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email_address": email_address,
+        "last_change_date": last_change_date,
+        "proposals": {},
+        "instruments": {}
     }
     sql = """
 SELECT
@@ -91,7 +101,15 @@ WHERE
     rows = cursor.fetchall()
     global_instruments = {}
     for row in rows:
-        (proposal_id, title, group_id, accepted_date, actual_end_date, actual_start_date, closed_date) = row
+        (
+            proposal_id,
+            title,
+            group_id,
+            accepted_date,
+            actual_end_date,
+            actual_start_date,
+            closed_date
+        ) = row
         data["proposals"][str(proposal_id)] = {
             "title": title,
             "group_id": group_id,
@@ -114,7 +132,7 @@ WHERE
         data["proposals"][str(proposal_id)]["instruments"] = cursor.fetchall()
         for i in data["proposals"][str(proposal_id)]["instruments"]:
             global_instruments[i] = 1
-    for intstrument_id in global_instruments.keys():
+    for instrument_id in global_instruments.keys():
         sql = """
 SELECT
   instrument_id,
@@ -133,7 +151,14 @@ WHERE
         rows = cursor.fetchall()
         if len(rows) != 1:
             error(dtype, "multiple instruments with id (%s)"%(str(instrument_id)), writer)
-        (instrument_id, instrument_name, last_change_date, name_short, eus_display_name, active_sw) = row[0]
+        (
+            instrument_id,
+            instrument_name,
+            last_change_date,
+            name_short,
+            eus_display_name,
+            active_sw
+        ) = rows[0]
         data["instruments"][str(instrument_id)] = {
             "instrument_id": instrument_id,
             "instrument_name": instrument_name,
