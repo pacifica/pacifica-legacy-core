8dd78c2fff0cdd22afde008e26bcd78bfc7c377d
dmlb2000 <dmlb2000@gmail.com>
2014-11-25 09:18:02 -0800

    add some stuff

--- src/pgsql_schema/updates/1.7-1.8.sql
+++ src/pgsql_schema/updates/1.7-1.8.sql
@@ -7,5 +7,5 @@ CREATE INDEX proposal_closed_date_idx on eus.proposals USING btree(closed_date);
 ALTER TABLE eus.instruments add column active_sw character default 'Y';
 CREATE INDEX instrument_active_sw_idx on eus.instruments USING btree(active_sw);
 
-update myemsl.system set value = '1.7' where key = 'schema_version';
+update myemsl.system set value = '1.8' where key = 'schema_version';
 
