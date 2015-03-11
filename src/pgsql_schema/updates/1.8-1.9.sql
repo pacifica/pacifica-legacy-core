5b9f96265e0b0cf4876b105d3a6d8477bad3e217
David Brown <david.brown@pnl.gov>
2015-03-11 11:41:44 -0700

    need some indexes here

--- src/pgsql_schema/updates/1.8-1.9.sql
+++ src/pgsql_schema/updates/1.8-1.9.sql
@@ -1,10 +1,14 @@
-DROP TABLE "emsl_staff_inst";
-CREATE TABLE "emsl_staff_inst" (
+DROP TABLE "eus"."emsl_staff_inst";
+CREATE TABLE "eus"."emsl_staff_inst" (
         "instrument_id" int4 NOT NULL,
         "person_id" int4 NOT NULL
 )
 WITHOUT OIDS;
-ALTER TABLE "emsl_staff_inst" OWNER TO "metadata_admins";
-GRANT SELECT ON "emsl_staff_inst" to "metadata_readers";
+
+CREATE INDEX emsl_staff_inst_i_idx on "eus"."emsl_staff_inst" using btree(instrument_id);
+CREATE INDEX emsl_staff_inst_p_idx on "eus"."emsl_staff_inst" using btree(person_id);
+
+ALTER TABLE "eus"."emsl_staff_inst" OWNER TO "metadata_admins";
+GRANT SELECT ON "eus"."emsl_staff_inst" to "metadata_readers";
 
 update myemsl.system set value = '1.8' where key = 'schema_version';
