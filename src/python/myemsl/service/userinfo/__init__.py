19f8f86f1c434e474733c99a13f778b1b1bc33f2
David Brown <david.brown@pnl.gov>
2015-03-11 13:24:03 -0700

    lets try doing things with userinfo

--- src/python/myemsl/service/userinfo/__init__.py
+++ src/python/myemsl/service/userinfo/__init__.py
@@ -48,8 +48,12 @@ def userinfo(user, dtype, writer):
     Currently it consists of the:
      * EUS user information
      * EUS proposals for the user
+     * EUS custodian table for instruments
      * EUS instruments for those proposals
     """
+    ##
+    # get user information from EUS
+    ##
     sql = """
 SELECT
   person_id,
@@ -80,9 +84,58 @@ WHERE
         "proposals": {},
         "instruments": {}
     }
+    global_instruments = {}
+    global_proposals = {}
+    ##
+    # This pulls the instruments from custodians and gets
+    # the associated proposals
+    ##
     sql = """
 SELECT
-  eus.proposals.proposal_id,
+  instrument_id
+FROM
+  eus.emsl_staff_inst
+WHERE
+  person_id = %(person_id)s
+    """
+    cursor = cnx.cursor()
+    cursor.execute(sql, {'person_id':person_id})
+    for instrument_id in cursor.fetchall():
+        global_instruments[instrument_id] = 1
+        sql = """
+SELECT
+  proposal_id
+FROM
+  eus.proposal_instruments
+WHERE
+  proposal_instruments.instrument_id = %(instrument_id)s
+        """
+        cursor = cnx.cursor()
+        cursor.execute(sql, {'instrument_id':instrument_id})
+        for proposal_id in cursor.fetchall():
+            global_proposals[proposal_id] = 1
+    ##
+    # Get the proposals the user is on
+    ##
+    sql = """
+SELECT
+  proposal_id
+FROM
+  eus.proposal_members
+WHERE
+  person_id = %(person_id)s
+    """
+    cursor = cnx.cursor()
+    cursor.execute(sql, {'person_id':person_id})
+    for proposal_id in cursor.fetchall():
+        global_proposals[proposal_id] = 1
+    ##
+    # get proposal information from EUS for all proposals
+    ##
+    for proposal_id in global_proposals.keys():
+        sql = """
+SELECT
+  proposal_id,
   title,
   group_id,
   accepted_date,
@@ -90,49 +143,45 @@ SELECT
   actual_start_date,
   closed_date
 FROM
-  eus.proposals,
-  eus.proposal_members
+  eus.proposals
 WHERE
-  proposal_members.proposal_id = proposals.proposal_id AND
-  proposal_members.person_id = %(person_id)d
-    """
-    cursor = cnx.cursor()
-    cursor.execute(sql, {'person_id':user})
-    rows = cursor.fetchall()
-    global_instruments = {}
-    for row in rows:
-        (
-            proposal_id,
-            title,
-            group_id,
-            accepted_date,
-            actual_end_date,
-            actual_start_date,
-            closed_date
-        ) = row
-        data["proposals"][str(proposal_id)] = {
-            "title": title,
-            "group_id": group_id,
-            "accepted_date": accepted_date,
-            "actual_end_date": actual_end_date,
-            "actual_start_date": actual_start_date,
-            "closed_date": closed_date,
-            "instruments": []
-        }
-        sql = """
+  proposal_id = %(proposal_id)d
+        """
+        cursor = cnx.cursor()
+        cursor.execute(sql, {'person_id':user})
+        for row in cursor.fetchall():
+            (
+                proposal_id,
+                title,
+                group_id,
+                accepted_date,
+                actual_end_date,
+                actual_start_date,
+                closed_date
+            ) = row
+            data["proposals"][str(proposal_id)] = {
+                "title": title,
+                "group_id": group_id,
+                "accepted_date": accepted_date,
+                "actual_end_date": actual_end_date,
+                "actual_start_date": actual_start_date,
+                "closed_date": closed_date,
+                "instruments": []
+            }
+            sql = """
 SELECT
   instrument_id
 FROM
   eus.proposal_instruments
 WHERE
   proposal_instruments.proposal_id = %(proposal_id)s
-        """
-        cursor = cnx.cursor()
-        cursor.execute(sql, {'proposal_id':proposal_id})
-        data["proposals"][str(proposal_id)]["instruments"] = cursor.fetchall()
-        for i in data["proposals"][str(proposal_id)]["instruments"]:
-            i = i[0]
-            global_instruments[i] = 1
+            """
+            cursor = cnx.cursor()
+            cursor.execute(sql, {'proposal_id':proposal_id})
+            data["proposals"][str(proposal_id)]["instruments"] = cursor.fetchall()
+            for i in data["proposals"][str(proposal_id)]["instruments"]:
+                i = i[0]
+                global_instruments[i] = 1
     for instrument_id in global_instruments.keys():
         sql = """
 SELECT
