05a5a13840a2c31ec7ed5ed3d730b2924d2f9ba7
dmlb2000 <dmlb2000@gmail.com>
2015-08-26 23:08:24 -0700

    okay added makefile bits for all this

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-libtoolize &&
+case `uname` in Darwin*) glibtoolize --copy ;;
+  *) libtoolize --copy ;; esac &&
 aclocal -I m4 &&
 automake --add-missing --copy --gnu &&
 autoconf --force &&
