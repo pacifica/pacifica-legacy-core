892e70e44e5e3dbec8a95efd837ecdbb88acdc73
dmlb2000 <dmlb2000@gmail.com>
2016-03-02 13:47:59 -0800

    fix up some name space things

--- src/pgsql_schema/updates/1.9-1.10.sql
+++ src/pgsql_schema/updates/1.9-1.10.sql
@@ -1,30 +1,30 @@
-CREATE INDEX  idx_group_types ON groups USING btree("type" ASC);
-ALTER TABLE groups CLUSTER ON idx_group_types;
-CREATE INDEX  idx_group_names ON groups USING btree("name" ASC);
+CREATE INDEX  idx_group_types ON "myemsl"."groups" USING btree("type" ASC);
+ALTER TABLE "myemsl"."groups" CLUSTER ON idx_group_types;
+CREATE INDEX  idx_group_names ON "myemsl"."groups" USING btree("name" ASC);
 
 
-ALTER TABLE "group_items" CLUSTER ON group_items_id_idx;
-CREATE INDEX  group_items_id_idx ON "group_items" USING btree(group_id ASC);
+CREATE INDEX  group_items_id_idx ON "myemsl"."group_items" USING btree(group_id ASC);
+ALTER TABLE "myemsl"."group_items" CLUSTER ON group_items_id_idx;
 
 
-CREATE INDEX  "idx_f_item_id" ON "files" USING btree(item_id ASC NULLS LAST);
-CREATE INDEX  "idx_f_filename" ON "files" USING btree("name" ASC NULLS LAST);
-CREATE INDEX  "idx_f_trans_id" ON "files" USING btree("transaction" DESC NULLS LAST);
-ALTER TABLE "files" CLUSTER ON "idx_f_trans_id";
+CREATE INDEX  "idx_f_item_id" ON "myemsl"."files" USING btree(item_id ASC NULLS LAST);
+CREATE INDEX  "idx_f_filename" ON "myemsl"."files" USING btree("name" ASC NULLS LAST);
+CREATE INDEX  "idx_f_trans_id" ON "myemsl"."files" USING btree("transaction" DESC NULLS LAST);
+ALTER TABLE "myemsl"."files" CLUSTER ON "idx_f_trans_id";
 
 
-ALTER TABLE "hashsums" ALTER COLUMN "item_id" SET NOT NULL;
-ALTER TABLE "hashsums" ADD PRIMARY KEY ("item_id");
-CREATE INDEX  "idx_hs_hashes" ON "hashsums" USING btree(hashsum ASC);
+ALTER TABLE "myemsl"."hashsums" ALTER COLUMN "item_id" SET NOT NULL;
+ALTER TABLE "myemsl"."hashsums" ADD PRIMARY KEY ("item_id");
+CREATE INDEX  "idx_hs_hashes" ON "myemsl"."hashsums" USING btree(hashsum ASC);
 
 
-CREATE INDEX  "idx_c_person_id_c" ON "cart" USING btree(person_id ASC);
-ALTER TABLE "cart" CLUSTER ON "idx_c_person_id_c";
+CREATE INDEX  "idx_c_person_id_c" ON "myemsl"."cart" USING btree(person_id ASC);
+ALTER TABLE "myemsl"."cart" CLUSTER ON "idx_c_person_id_c";
 
 
-CREATE INDEX  "idx_ci_items" ON "cart_items" USING btree(item_id);
-CREATE INDEX  "idx_ci_carts" ON "cart_items" USING btree(cart_id);
-ALTER TABLE "cart_items" CLUSTER ON "idx_ci_carts";
+CREATE INDEX  "idx_ci_items" ON "myemsl"."cart_items" USING btree(item_id);
+CREATE INDEX  "idx_ci_carts" ON "myemsl"."cart_items" USING btree(cart_id);
+ALTER TABLE "myemsl"."cart_items" CLUSTER ON "idx_ci_carts";
 
 
 ALTER TABLE "myemsl"."ingest_state" 
@@ -32,7 +32,7 @@ ALTER TABLE "myemsl"."ingest_state"
 	ADD COLUMN "updated" timestamptz NOT NULL;
 	
 	
-CREATE INDEX  idx_is_job_id ON "ingest_state" USING btree(jobid ASC);
+CREATE INDEX  idx_is_job_id ON "myemsl"."ingest_state" USING btree(jobid ASC);
 
 ALTER TABLE "myemsl"."files"
   ADD COLUMN "mtime" TIMESTAMP WITHOUT TIME ZONE;
