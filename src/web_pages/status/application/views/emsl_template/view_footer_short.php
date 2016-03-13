6c5b196a25ec4ed3dff0b525006cb3442a86beb5
dmlb2000 <dmlb2000@gmail.com>
2016-03-12 20:30:54 -0800

    remove the status page moving to another git submodule
    This removes the status page and we are moving it to a new pacifica
    submodule github.com/EMSL-MSC/pacificia-upload-status

--- src/web_pages/status/application/views/emsl_template/view_footer_short.php
+++
@@ -1,6 +0,0 @@
-<footer class="short">
-    <section id="contact_info">
-        <p><a class="email" href="mailto:emsl@pnnl.gov">EMSL, The Environmental Molecular Sciences Laboratory</a></p>
-        <div id="last_update_timestamp" style="">Last Source Update: <?= $this->last_update_time->format('n/j/Y g:i a') ?></div>
-    </section>
-</footer>
--- src/web_pages/status/application/views/pnnl_template/view_footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-  //$this->load->view('pnnl_template/leftnav');
-//  var_dump($infoData);
-//  $this->load->view('pnnl_template/rightnav_search',$infoData);
-  $this->load->view('pnnl_template/footer');
-?>
